1e5c96a250ec463779681cbe509ab7ca712c0634
Martin Cai <martin_cai@hotmail.com>
2014-11-05 21:02:30 -0800

    Publish plug-in v1 for Joomla!

---
+++ Joomla!/bingmaps/plugin.js
@@ -0,0 +1,73 @@
+﻿( function() {
+﻿/**
+ * Copyright (C) Microsoft Open Technologies (Shanghai) Company Limited
+ *
+ * GNU General Public License, version 2 (GPL-2.0)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+  CKEDITOR.plugins.add( 'bingmaps',
+  {
+    init: function( editor )
+    {
+      CKEDITOR.dialog.add( 'BingMapsDialog', function( editor ) {
+        return {
+            title: '插入必应地图',
+            minWidth: 600,
+            minHeight: 300,
+            onOk: function()
+            {
+                var editor = this.getParentEditor();
+                var mapsrc = document.getElementById('bingmapshtml_param_1').contentWindow.document.getElementById("bingmapsurl").value;
+                var location = document.getElementById('bingmapshtml_param_1').contentWindow.document.getElementById("location").value;
+                if (location == null || location.length == 0) {
+                    alert('请输入地址');
+                }
+                if (mapsrc) {
+                    var vmapsrc = "http://dev.ditu.live.com/REST/v1/Imagery/Map/Road/" + mapsrc;
+                    var strimg1 = '<img src="' + vmapsrc + '" border=0 />';
+                    editor.insertHtml(strimg1);
+                }
+            },
+            contents: [
+                {
+                    id: 'tab-basic',
+                    label: 'bingmaps',
+                    elements: [
+                        {
+                            type: 'html',
+                            id: 'bingmapshtml',
+                            html: "<iframe id=\"bingmapshtml_param_1\" frameborder=\"0\" style=\"width:625px;height:455px;\" scrolling=\"no\" onload=\"this.contentWindow.document.body.style.width=\'600px\';this.onload=null\" src=\"http://" + document.domain + "/plugins/editors/ckeditor/plugins/bingmaps/static/template/insertmap.html\"></iframe>",
+                        },
+                    ]
+                },
+            ],
+        };
+      });
+
+      editor.addCommand( 'BingMaps', new CKEDITOR.dialogCommand( 'BingMapsDialog' ) );
+
+      editor.ui.addButton( 'BingMaps',
+      {
+        label: '必应地图',
+        command: 'BingMaps',
+        icon: this.path + 'static/img/icon.png'
+      } );
+    },
+  } );
+  
+} )();
---
+++ Joomla!/bingmaps/static/js/maps7.0.js
@@ -0,0 +1,72 @@
+﻿﻿/**
+ * Copyright (C) Microsoft Open Technologies (Shanghai) Company Limited
+ *
+ * GNU General Public License, version 2 (GPL-2.0)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+ 
+var bmapkey = 'Bing Maps Key';
+var beginLatitude = 39.904029;
+var beginLongitude = 116.407524;
+var beginZoom = 10;
+var bmapzoom = 17;
+var bmapwidth = 600;
+var bmapheight = 300;
+var map = null;
+
+function GetMap()
+{
+    // Initialize the map
+    map = new Microsoft.Maps.Map(document.getElementById("myMap"), {credentials: bmapkey});
+
+    // Define the pushpin location
+    var loc = new Microsoft.Maps.Location(beginLatitude, beginLongitude);
+
+    // Center the map on the location
+    map.setView({center: loc, zoom: beginZoom});
+    // Load map search module
+    Microsoft.Maps.loadModule('Microsoft.Maps.Search');
+}
+
+function FindAndAddPin(location)
+{
+    if (location == null || location.length == 0) {
+        alert('请输入地址');
+    } else {
+        try {
+            var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+            searchManager.geocode({where: location, count: 10, callback: geocodeCallback});
+        }
+        catch (e) {
+            alert(e.message);
+        }
+    }
+}
+
+function geocodeCallback(geocodeResult, userData)
+{
+    var location = geocodeResult.results[0].location;
+    var name = geocodeResult.results[0].name;
+    //set value
+    $('#bingmapsurl').val(location.latitude + ',' + location.longitude + '/' + bmapzoom + '?mapSize=' + bmapwidth + ',' + bmapheight + '&pushpin=' + location.latitude + ',' + location.longitude + ';21;' + name + '&key=' + bmapkey);
+    //new map pushpin and reset
+    var loc1 = new Microsoft.Maps.Location(location.latitude, location.longitude);
+    var pin = new Microsoft.Maps.Pushpin(loc1);
+    map.entities.push(pin);
+    map.setView({center: loc1, zoom: bmapzoom});
+}
+
